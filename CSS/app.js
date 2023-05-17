c47fdab9fb1182b2bf7f134618256a72fb5a3e67
deshmukh <deshmukhsomesh7777@gmail.com>
2023-05-18 03:10:04 +0530

    flex box

---
+++ CSS/app.js
@@ -0,0 +1,34 @@
+// DOM Manipulation
+
+
+
+// GetElementById()
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// const title1 = document.getElementById('somesh');
+// console.log(title1);
+
+
+// GetElementByClassName()
+
+// const listitem = document.getElementsByClassName('list-items');
+// console.log(listitem);
+
+
+// getElementsByTagName()
+
+// const  listitems = document.getElementsByTagName('div');
+// console.log(listitems);
+
+
+// querySelector()
+
+// const container = document.querySelector('div');
+// console.log(container);
+
+// querySelectorAll()
+
+const container  = document.querySelectorAll('div');
+console.log(container);
